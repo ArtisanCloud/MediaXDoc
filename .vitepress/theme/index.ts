47eb7b4f3a01356019cdaa1ac84e29ae0a5b4cf0
Matrix-X <hbj418@gmail.com>
2025-05-10 21:51:32 +0800

    feat(action): default theme

--- .vitepress/theme/index.ts
+++ .vitepress/theme/index.ts
@@ -1,20 +1,23 @@
-// .vitepress/theme/index.ts
-import DefaultTheme from 'vitepress/theme'
-import type { Theme } from 'vitepress'
-import { theme, useOpenapi } from 'vitepress-openapi/client'
-import 'vitepress-openapi/dist/style.css'
-import spec from '../../src/openapi.json' with { type: 'json' } 
+// // .vitepress/theme/index.ts
+// import DefaultTheme from 'vitepress/theme'
+// import type { Theme } from 'vitepress'
+// import { theme, useOpenapi } from 'vitepress-openapi/client'
+// import 'vitepress-openapi/dist/style.css'
+// import spec from '../../src/openapi.json' with { type: 'json' }
 
+// export default {
+//     extends: DefaultTheme,
+//     async enhanceApp({ app }) {
+//         useOpenapi({
+//             spec,
+//             config: {
+//                 // Custom theme configuration...
+//             },
+//         })
+//         theme.enhanceApp({ app })
+//     }
+// } satisfies Theme
 
-export default {
-    extends: DefaultTheme,
-    async enhanceApp({ app }) {
-        useOpenapi({ 
-            spec, 
-            config: { 
-                // Custom theme configuration...
-            }, 
-        }) 
-        theme.enhanceApp({ app }) 
-    }
-} satisfies Theme
+import DefaultTheme from "vitepress/theme";
+
+export default DefaultTheme;
